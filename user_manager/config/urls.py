2b5aaac9efc154be955912d595e3c0350aefd662
ario <cybera.3s@gmail.com>
2022-09-09 15:27:17 +0430

    add grpc service handlers

--- user_manager/config/urls.py
+++ user_manager/config/urls.py
@@ -1,21 +1,8 @@
-"""config URL Configuration
+from grpc_utils.user_proto import user_pb2_grpc
+from users.services import UserService
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+urlpatterns = []
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+
+def grpc_handlers(server):
+    user_pb2_grpc.add_UserControllerServicer_to_server(UserService.as_servicer(), server)
