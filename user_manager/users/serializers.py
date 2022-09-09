ce1ac57152d671e01096f0d57ccb48e7107a3fc3
ario <cybera.3s@gmail.com>
2022-09-09 15:30:58 +0430

    add UserProtoSerializer

---
+++ user_manager/users/serializers.py
@@ -0,0 +1,10 @@
+from .models import User
+from django_grpc_framework import proto_serializers
+from grpc_utils.user_proto import user_pb2
+
+
+class UserProtoSerializer(proto_serializers.ModelProtoSerializer):
+    class Meta:
+        model = User
+        proto_class = user_pb2.User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
